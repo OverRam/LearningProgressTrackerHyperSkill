10b98eaed4ada1da51cc08c1174fa7cf73235f81
OverRam <yugini666@gmail.com>
2022-02-06 23:39:44 +0100

    Adding method menu and refactor method run.

--- Learning Progress Tracker/task/src/tracker/UserInterFace.java
+++ Learning Progress Tracker/task/src/tracker/UserInterFace.java
@@ -1,24 +1,38 @@
 package tracker;
 
-import java.util.HashMap;
+import java.util.Scanner;
 
 public class UserInterFace {
     DataBase dataBase;
+    Scanner sc = new Scanner(System.in);
 
     void run() {
         dataBase = new DataBase();
         System.out.println("Learning Progress Tracker");
+        String userInp = sc.nextLine().strip();
+//        String userInp = "add students";
 
+        while (!userInp.equalsIgnoreCase("exit")) {
+            menu(userInp, dataBase);
+            userInp = sc.nextLine().strip();
+        }
+        System.out.println("Bye!");
 
     }
 
-    private boolean addPerson(String studentData) {
-        HashMap<String, String> structuredStudentData = DataHandler.dataStructuring(studentData);
-
-        if (DataHandler.checkStudentData(structuredStudentData)) {
-            dataBase.addStudent(structuredStudentData);
-            return true;
+    private void menu(String userInp, DataBase dataBase) {
+        switch (userInp) {
+            case "add students":
+                AddingPerson.addStudentsDataToDataBase(dataBase);
+                break;
+            case "":
+                System.out.println("No input");
+                break;
+            case "back":
+                System.out.println("Enter 'exit' to exit the program");
+                break;
+            default:
+                System.out.println("Unknown command!");
         }
-        return false;
     }
 }
