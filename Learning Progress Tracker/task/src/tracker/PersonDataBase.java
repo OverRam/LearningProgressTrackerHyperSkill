7f9d54c87325cf6ccec699b44c5cb10ff56ba2bd
OverRam <yugini666@gmail.com>
2022-04-03 16:15:24 +0200

    Implements and methods: notifyStudentsDoneCurse, setStatusNotificationSend, setStudentDoneCures, MessageToStudentDoneCourse

--- Learning Progress Tracker/task/src/tracker/PersonDataBase.java
+++ Learning Progress Tracker/task/src/tracker/PersonDataBase.java
@@ -2,7 +2,10 @@ package tracker;
 
 import tracker.Users.Student;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PersonDataBase {
     private int idPerson = 9999;
@@ -22,4 +25,71 @@ public class PersonDataBase {
     public Student getStudent(int idStudent) {
         return studentDatabase.get(idStudent);
     }
+
+    void notifyStudentsDoneCurse() {
+        Map<Student, List<LanguagesEnum>> doneCuresByStudent = setStudentDoneCures();
+
+        doneCuresByStudent.forEach(((student, courseList) -> courseList.forEach(courseName -> {
+            MessageToStudentDoneCourse(student, courseName.toString());
+            setStatusNotificationSend(student, courseName);
+        })));
+
+        System.out.printf("Total %d students have been notified.%n", doneCuresByStudent.size());
+
+    }
+
+    private void setStatusNotificationSend(Student student, LanguagesEnum courseName) {
+
+        switch (courseName) {
+            case JAVA:
+                student.setNotificationDoneJavaSent();
+                break;
+            case DSA:
+                student.setNotificationDoneDSASent();
+                break;
+            case DATABASE:
+                student.setNotificationDoneDatabasesSent();
+                break;
+            case SPRING:
+                student.setNotificationDoneSpringSent();
+                break;
+            default:
+                throw new RuntimeException("Wrong course name.");
+        }
+    }
+
+    private Map<Student, List<LanguagesEnum>> setStudentDoneCures() {
+        HashMap<Student, List<LanguagesEnum>> doneCuresByStudent = new HashMap<>();
+
+        studentDatabase.values().forEach(s -> {
+            List<LanguagesEnum> addedCourseList = new ArrayList<>();
+
+            if (s.isCompleteJavaCourse() && !s.isNotificationDoneJavaSent()) {
+                addedCourseList.add(LanguagesEnum.JAVA);
+            }
+            if (s.isCompleteDSACourse() && !s.isNotificationDoneDSASent()) {
+                addedCourseList.add(LanguagesEnum.DSA);
+            }
+            if (s.isCompleteDatabasesCourse() && !s.isNotificationDoneDatabasesSent()) {
+                addedCourseList.add(LanguagesEnum.DATABASE);
+            }
+            if (s.isCompleteSpringCourse() && !s.isNotificationDoneSpringSent()) {
+                addedCourseList.add(LanguagesEnum.SPRING);
+            }
+
+            if (addedCourseList.size() > 0) {
+                doneCuresByStudent.put(s, addedCourseList);
+            }
+        });
+
+        return doneCuresByStudent;
+    }
+
+    void MessageToStudentDoneCourse(Student student, String courseName) {
+        System.out.printf("To: %s%n" +
+                        "Re: Your Learning Progress%n" +
+                        "Hello, %s %s! You have accomplished our %s course!%n",
+                student.getEmail(), student.getFirstName(), student.getLastName(), courseName);
+    }
+
 }
