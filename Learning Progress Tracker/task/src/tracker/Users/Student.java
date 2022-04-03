4670aebae854d237a2f300a9ba607c691edc5cdd
OverRam <yugini666@gmail.com>
2022-04-03 16:19:54 +0200

    Implement methods getters

--- Learning Progress Tracker/task/src/tracker/Users/Student.java
+++ Learning Progress Tracker/task/src/tracker/Users/Student.java
@@ -1,35 +1,28 @@
 package tracker.Users;
 
+import tracker.Statistics;
+
 public class Student {
 
     private final String firstName;
     private final String lastName;
     private final String email;
     private final int id;
+
     private int javaPoints = 0;
     private int dsaPoints = 0;
     private int databasesLangPoints = 0;
     private int springPoints = 0;
+
     private int javaCompletedTasks = 0;
     private int dsaCompletedTasks = 0;
     private int databasesLangCompletedTasks = 0;
     private int springCompletedTasks = 0;
 
-    public int getJavaCompletedTasks() {
-        return javaCompletedTasks;
-    }
-
-    public int getDsaCompletedTasks() {
-        return dsaCompletedTasks;
-    }
-
-    public int getDatabasesLangCompletedTasks() {
-        return databasesLangCompletedTasks;
-    }
-
-    public int getSpringCompletedTasks() {
-        return springCompletedTasks;
-    }
+    private boolean notificationDoneJavaSent = false;
+    private boolean notificationDoneSpringSent = false;
+    private boolean notificationDoneDSASent = false;
+    private boolean notificationDoneDatabasesSent = false;
 
     public Student(String firstName, String lastName, String email, int id) {
         this.firstName = firstName;
@@ -66,12 +59,76 @@ public class Student {
         }
     }
 
-    //format java, dsa, dB, spring
-    public void addAllPointsInFormat(int java, int dsa, int db, int spring) {
-        addPointsJava(java);
-        addPointsDSA(dsa);
-        addPointsDatabasesLang(db);
-        addPointsSpring(spring);
+    public boolean isCompleteJavaCourse() {
+        return javaPoints >= Statistics.JAVA_MAX_POINTS;
+    }
+
+    public boolean isCompleteSpringCourse() {
+        return springPoints >= Statistics.SPRING_MAX_POINTS;
+    }
+
+    public boolean isCompleteDSACourse() {
+        return dsaPoints >= Statistics.DSA_MAX_POINTS;
+    }
+
+    public boolean isCompleteDatabasesCourse() {
+        return databasesLangPoints > Statistics.DATABASE_MAX_POINTS;
+    }
+
+    public void setNotificationDoneJavaSent() {
+        if (isCompleteJavaCourse()) {
+            notificationDoneJavaSent = true;
+        }
+    }
+
+    public void setNotificationDoneSpringSent() {
+        if (isCompleteSpringCourse()) {
+            this.notificationDoneSpringSent = true;
+        }
+    }
+
+    public void setNotificationDoneDSASent() {
+        if (isCompleteDSACourse()) {
+            this.notificationDoneDSASent = true;
+        }
+    }
+
+    public void setNotificationDoneDatabasesSent() {
+        if (isCompleteDatabasesCourse()) {
+            this.notificationDoneDatabasesSent = true;
+        }
+    }
+
+    public boolean isNotificationDoneJavaSent() {
+        return notificationDoneJavaSent;
+    }
+
+    public boolean isNotificationDoneSpringSent() {
+        return notificationDoneSpringSent;
+    }
+
+    public boolean isNotificationDoneDSASent() {
+        return notificationDoneDSASent;
+    }
+
+    public boolean isNotificationDoneDatabasesSent() {
+        return notificationDoneDatabasesSent;
+    }
+
+    public int getJavaCompletedTasks() {
+        return javaCompletedTasks;
+    }
+
+    public int getDsaCompletedTasks() {
+        return dsaCompletedTasks;
+    }
+
+    public int getDatabasesLangCompletedTasks() {
+        return databasesLangCompletedTasks;
+    }
+
+    public int getSpringCompletedTasks() {
+        return springCompletedTasks;
     }
 
     public int getJavaPoints() {
